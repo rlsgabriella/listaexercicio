3769383c37753c737473e03a2a14cb7c3f6d4ab6
Gabriella <rlsgabriella@gmail.com>
2024-07-07 15:53:09 -0300

    conteudo lista, estudar e ajustar

---
+++ script.js
@@ -0,0 +1,13 @@
+// questão 04//
+
+var idade = parseInt(prompt('Digite sua idade'))
+function verificaIdade(idade) {
+        if (idade < 16) {
+            console.log('Não eleitor')
+        } else if ('idade >= 18 && idade < = 65') {
+            consolelog('Eleitor')
+        } else if (idade >= 16 && idade >= 18 || idade > 65)
+            console.log('Vota se quiser')
+    }
+
+verificaIdade(idade)
